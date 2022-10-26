c1f4c0fbaace7308fb4d2b539f7c678189bc7c29
Chelsea Troy <ctroy@mozilla.com>
2022-10-26 20:55:50 +0000

    Document production location of data validation code

--- nightly-job/data_validation_job.py
+++ nightly-job/data_validation_job.py
@@ -6,6 +6,12 @@ from collections import namedtuple
 
 from data_validation import retrieve_data_validation_metrics, record_validation_results
 
+## THIS IS A STAGING LOCATION FOR THIS JOB
+## CHANGING IT HERE WILL NOT CHANGE THE ACTUAL FUNCTIONALITY IN PROD
+## THIS IS WHERE THE PROD VERSION LIVES:
+## https://github.com/mozilla/docker-etl/blob/main/jobs/search-term-data-validation/src
+## PLEASE MAKE CHANGES TO THAT, AND THEN MAKE MATCHING CHANGES IN THIS ONE
+
 parser = argparse.ArgumentParser(description="Validate Recent Search Input Against Historical Norms",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--data_validation_origin", help="Origin table for data validation metrics")
