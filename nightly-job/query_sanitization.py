1d1b3a2315259e566e8c90d891962249824f0d56
Chelsea Troy <chelsea.dommert@gmail.com>
2026-02-24 12:54:39 -0600

    Add comment explaining what we learned about the impact of process start method on model memory requirements

--- nightly-job/query_sanitization.py
+++ nightly-job/query_sanitization.py
@@ -105,6 +105,19 @@ def detect_pii(series, census_surnames, nlp, n_process=1):
         "checkpoint_delta_seconds": 0,
     })
 
+    '''
+    The n_process arg used below specifies the number of processes for the spaCy NLP model. 
+    It allows us to parallelize the slowest part of this job across cores.
+    On Mac/OSX (which defaults to the `spawn` start setting) or CUDA (which can only use `spawn`),
+    this has bigger memory implications, as that requires a copy of the model data per core.
+    A Linux box like the one we use in prod defaults instead to the `fork` start setting,
+    which can allow a shared copy of the model data in the parent process.
+    
+    TAKEAWAY: Don't set n_process above 1 on a Mac or a GPU machine.
+    
+    spaCy doc on data copying behavior per start method: https://spacy.io/usage/processing-pipelines
+    Python doc on process start methods: https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods
+    '''
     docs = list(nlp.pipe(texts_needing_nlp, n_process=n_process))
 
     now = datetime.now(timezone.utc)
